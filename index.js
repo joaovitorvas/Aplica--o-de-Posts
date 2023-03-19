19fa21faf927de64a6db0c8a67a47b30e1deda90
joaovitorvas <joaovitorvasc.2307@outlook.com>
2023-03-19 11:08:35 -0300

    Função deletar

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ const Post = require('./models/Post');
         });
     })
 
+    app.get('/deletar/:id', function(req, res){
+        Post.destroy({where: {'id': req.params.id}}).then(function(){
+            res.send('Postagem deletada com sucesso!')
+        }).catch(function(erro){
+            res.send('Essa postagem não existe!')
+        });
+    });
+
 app.listen(8081, function(){
     console.log('Server rodando na url http://localhost:8081');
 });
